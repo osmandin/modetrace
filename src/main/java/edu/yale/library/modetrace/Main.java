cd1a178a576cd012045935e60055969e4b4497eb
osmandin <osmandin@gmail.com>
2014-02-11 16:44:22 -0500

    Add functionality for reading node summary.

--- src/main/java/edu/yale/library/modetrace/Main.java
+++ src/main/java/edu/yale/library/modetrace/Main.java
@@ -4,6 +4,12 @@ import org.slf4j.*;
 import org.apache.commons.cli.*;
 import org.apache.commons.lang3.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Run application
  */
@@ -14,30 +20,40 @@ public class Main {
 
     private String cacheStorePath = "";
 
-    public static void main(String[] args) {
+    private List<String> keys = new ArrayList(); //TODO
+
+    private final MODE operation;
+
+    public static void main(String[] args) throws FileNotFoundException {
         final Main main = newInstance(args);
-        switch (main.cacheStore()) {
-            case FileCacheStore:
-                LOGGER.debug("Printing file cache store contents.");
-                printFileCacheStoreArtifacts(main.getCacheStorePath());
+        switch (main.operation) {
+            case READ_NODE:
+                switch (main.cacheStore) {
+                    case FileCacheStore:
+                        printNodesFileData(main.getCacheStorePath(), main.keys);
+                        break;
+                    default:
+                        LOGGER.error("Unrecognized store.");
+                        break;
+                }
+                break;
+            case READ_FOLDER:
+                switch (main.cacheStore) {
+                    case FileCacheStore:
+                        printFileCacheStoreArtifacts(main.getCacheStorePath());
+                        break;
+                    default:
+                        LOGGER.error("Unrecognized store.");
+                        break;
+                }
                 break;
             default:
-                System.out.println("Currently unrecognized store.");
+                LOGGER.error("Not yet . . ."); //
                 break;
         }
     }
 
-    /**
-     * Prints file-node mapping
-     *
-     * @param path
-     */
-    public static void printFileCacheStoreArtifacts(String path) {
-        String output = SimpleFileStoreReader.readFolder(path);
-        System.out.println(output);
-    }
-
-    private static Main newInstance(final String args[]) throws IllegalArgumentException {
+    private static Main newInstance(final String args[]) throws IllegalArgumentException, FileNotFoundException {
         Main m = null;
         String type, location = "";
         final Options ops = createOptions();
@@ -51,11 +67,14 @@ public class Main {
             if (cli.hasOption("t") && cli.hasOption("f")) {
                 type = cli.getOptionValue("t");
                 location = cli.getOptionValue("f");
-
                 if (EnumUtils.isValidEnum(CacheStore.class, type) == false) {
                     throw new IllegalArgumentException("Invalid or unrecognized store : " + type);
                 }
-                m = new Main(CacheStore.valueOf(type), location);
+                if (cli.hasOption("n")) {
+                    m = new Main(MODE.READ_NODE, CacheStore.valueOf(type), location, getKeys(cli.getOptionValue("n")));
+                } else {
+                    m = new Main(MODE.READ_FOLDER, CacheStore.valueOf(type), location);
+                }
             } else {
                 LOGGER.error("Error running program.");
                 printUsage(ops);
@@ -65,6 +84,28 @@ public class Main {
             p.printStackTrace();
         }
         return m;
+    }
+
+    /**
+     * Prints file-node mapping
+     *
+     * @param path
+     */
+    public static void printNodesFileData(final String path, final List<String> keys) {
+        LOGGER.debug("Printing node(s) data");
+        String output = SimpleFileStoreReader.readNodes(path, keys);
+        System.out.println(output); //TODO
+    }
+
+    /**
+     * Prints file-node mapping
+     *
+     * @param path
+     */
+    public static void printFileCacheStoreArtifacts(final String path) {
+        LOGGER.debug("Printing file cache store data");
+        String output = SimpleFileStoreReader.readFolder(path);
+        System.out.println(output); //TODO
     }
 
     @SuppressWarnings("static-access")
@@ -79,6 +120,9 @@ public class Main {
         ops.addOption(OptionBuilder.withArgName("type").withDescription(
                 "Type of the Infinispan store.")
                 .withLongOpt("type").hasArg().create('t'));
+        ops.addOption(OptionBuilder.withArgName("nodes").withDescription(
+                "Text file with nodes.")
+                .withLongOpt("nodes").hasArg().create('n'));
         return ops;
     }
 
@@ -87,6 +131,37 @@ public class Main {
         formatter.printHelp("java -jar target/modetrace-jar-with-dependencies.jar -t {FileCacheStore} -f {/tmp/fcrepo4-data}", options);
     }
 
+    //TODO
+    static List getKeys(final String nodeFilePath) throws FileNotFoundException {
+        List<String> list = new ArrayList();
+        //e.g. ("87a0a8c317f1e7/jcr:system/jcr:nodeTypes/mix:lastModified");
+        Scanner s = new Scanner(new File(nodeFilePath));
+        while (s.hasNext()) {
+            list.add(s.next());
+        }
+        return list;
+    }
+
+    public String getCacheStorePath() {
+        return cacheStorePath;
+    }
+
+    public Main(MODE operation, CacheStore cacheStore, String path) {
+        this.operation = operation;
+        this.cacheStore = cacheStore;
+        this.cacheStorePath = path;
+    }
+
+    public Main(MODE operation, CacheStore cacheStore, String path, List<String> keys) {
+        this.operation = operation;
+        this.cacheStore = cacheStore;
+        this.cacheStorePath = path;
+        this.keys = keys;
+    }
+
+    private enum MODE {
+        READ_FOLDER, READ_NODE;
+    }
 
     private enum CacheStore {
 
@@ -107,18 +182,5 @@ public class Main {
         void setType(String type) {
             this.type = type;
         }
-    }
-
-    public CacheStore cacheStore() {
-        return cacheStore;
-    }
-
-    public Main(CacheStore cacheStore, String path) {
-        this.cacheStore = cacheStore;
-        this.cacheStorePath = path;
-    }
-
-    public String getCacheStorePath() {
-        return cacheStorePath;
     }
 }
--- src/main/java/edu/yale/library/modetrace/SimpleFileStoreReader.java
+++ src/main/java/edu/yale/library/modetrace/SimpleFileStoreReader.java
@@ -1,28 +1,25 @@
 package edu.yale.library.modetrace;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.collections.IteratorUtils;
 import org.apache.commons.io.FileUtils;
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
-import org.infinispan.configuration.cache.FileCacheStoreConfiguration;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.schematic.SchematicEntry;
 import org.infinispan.schematic.document.Document;
 import org.infinispan.schematic.document.Json;
 import org.infinispan.schematic.internal.CacheSchematicDb;
-import org.infinispan.schematic.internal.document.BasicDocument;
 import org.infinispan.schematic.internal.document.BsonReader;
-import org.infinispan.schematic.internal.document.ImmutableField;
 import org.infinispan.transaction.lookup.DummyTransactionManagerLookup;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.ByteArrayInputStream;
 
 
 public class SimpleFileStoreReader {
@@ -33,83 +30,129 @@ public class SimpleFileStoreReader {
     private static Cache<String, SchematicEntry> ispnCache;
     private static EmbeddedCacheManager cacheManager;
 
-    private static final String ISPN_CACHE = "documents";
-    private static final String KEY_ID = "key"; //TODO
-    private static final String FCREPO_TEST_FILE_DIRECTORY = "target/test-classes/files";
-
-
+    private static final String ISPN_CACHE = "documents"; //TODO should be fcrep4-data or something
 
     public static String readFolder(final String path) {
-        return doReadFolder(path);
+        List<String> keys = doReadFolderAsList(path);
+        return doReadNodes(path, keys);
     }
 
-    private static String doReadFolder(final String path) {
-        init(FCREPO_TEST_FILE_DIRECTORY);
+    public static String readNodes(final String path, List<String> keys) {
+        return doReadNodes(path, keys);
+    }
+
+    private static String doReadNodes(final String path, List<String> keys) {
+        initCacheStore(path);
         SimpleFileStoreReader s = new SimpleFileStoreReader();
-        return s.testFedoraRootNodeFile();
+        return s.readNodeEntry(keys);
+    }
+
+    private static List<String> doReadFolderAsList(final String path) {
+        return BsonReaderUtil.readNodeFolderAsList(path);
     }
 
     /**
-     * Init once
-     * @param FCREPO_TEST_FILE_DIRECTORY
+     * For now reads only (id and content type), but can easily read the full document.
+     * Perhaps also return file name.
+     *
+     * @param nodes
+     * @return
      */
-    public static void init(final String FCREPO_TEST_FILE_DIRECTORY) {
+    public String readNodeEntry(final List<String> nodes) {
 
-        LOGGER.debug("Init SimpleFileStoreReader");
+        long unableToReadCount = 0;
+        StringBuffer sb = new StringBuffer();
+        org.infinispan.Cache cache = schematicDB.getCache();
+        for (String n : nodes) {
+            cache.get(n);
+            final SchematicEntry schematicEntry = schematicDB.get(n);
+            if (schematicEntry == null) { //TODO
+                unableToReadCount++;
+                //LOGGER.debug("Unable to read:" + n );
+                //problem with some escape chars e.g in http://fedora.info/definitions
+                continue;  //FIXME
+            }
+            final Document entryMetadata = schematicEntry.getMetadata();
+            sb.append(Json.write(entryMetadata) + System.getProperty("line.separator"));
+        }
+        LOGGER.debug("Cache size=" + cache.size());
+        LOGGER.debug("Unable to read=" + unableToReadCount);
+        return sb.toString();
+    }
+
+    /**
+     * Reads file using org.infinispan.schematic.internal.document.BsonReader.
+     * TODO Ends up reading only first few hundred bytes. merge files for filecachestore
+     * TODO replace Commons FileUtils
+     */
+    private static final class BsonReaderUtil {
+
+        private static String readNodeFile(final byte[] bytes) {
+            BsonReader reader = new BsonReader();
+            String documentString = "NOT READ";
+            try {
+                final Document document = reader.read(new ByteArrayInputStream(bytes));
+                documentString = document.toString();
+                return documentString;
+            } catch (NullPointerException e) {
+                throw e;
+            } catch (IOException io) {
+                //ignore for now
+            }
+            return documentString;
+        }
+
+        private static List<String> readNodeFolderAsList(final String path) {
+            StringBuilder sb = new StringBuilder();
+            List<String> data = new ArrayList<String>();
+            final File dir = new File(path);
+            final File[] files = dir.listFiles();
+            for (final File f : files) {
+                try {
+                    final byte[] bytes = FileUtils.readFileToByteArray(f);
+                    data.add(filterNodeDataWithoutFileId(f.getName(), readNodeFile(bytes)));
+                } catch (NullPointerException e) {
+                    throw e;
+                } catch (Exception e) {
+                    continue; //ignore for now
+                }
+            }
+            return data;
+        }
+
+        /**
+         * TODO temporary
+         *
+         * @param file
+         * @param str
+         * @return
+         */
+        private static String filterNodeDataWithoutFileId(final String file, final String str) {
+            if (!str.contains(":")) { // no node data
+                return "{" + file + ":" + str + "}" + "\n";
+            }
+            int startIndex = 8;
+            int endIndex = str.length() - 12;
+            String modifiedString = str.replace(": null }", "");
+            modifiedString = str.substring(startIndex, endIndex);
+            //LOGGER.debug("Str:" + modifiedString);
+            return modifiedString;
+        }
+    }
+
+    /**
+     * TODO Init once, clean up etc
+     *
+     * @param FCREPO_DIRECTORY
+     */
+    public static void initCacheStore(final String FCREPO_DIRECTORY) {
+
         ConfigurationBuilder cfgBuilder = new ConfigurationBuilder();
         cfgBuilder.invocationBatching().enable().transaction()
                 .transactionManagerLookup(new DummyTransactionManagerLookup());
-        FileCacheStoreConfiguration cfg = cfgBuilder.loaders().addFileCacheStore().location(FCREPO_TEST_FILE_DIRECTORY).create();
+        cfgBuilder.loaders().addFileCacheStore().location(FCREPO_DIRECTORY).create();
         cacheManager = TestCacheManagerFactory.createCacheManager(cfgBuilder);
         ispnCache = cacheManager.getCache(ISPN_CACHE);
         schematicDB = new CacheSchematicDb(ispnCache);
     }
-
-    public String testFedoraRootNodeFile() {
-
-        LOGGER.debug("Reading file node");
-
-        final String key = "87a0a8c7505d64/"; //Fedora root node
-        final SchematicEntry schematicEntry = schematicDB.get(key);
-
-        /* Test whether the document can be read in the first place */
-        Document document = schematicEntry.getContentAsDocument();
-
-        boolean SKIP = true; //TODO remove
-
-        /* Validate root node */
-        final Document entryMetadata = schematicEntry.getMetadata();
-
-        if (SKIP)
-            return Json.write(entryMetadata);
-
-
-        /* Validate children */
-        Document children = document.getDocument("children");
-
-        /* Verify a particular child, i.e a child node of Fedora root node */
-        final String childNodeKeyString = "87a0a8c7505d644205a567-ac5f-41a7-b16f-66258bb54a53";
-        final String childNodeKeyName = "FedoraDatastreamsTest63";
-
-        Iterator<Document.Field> fieldsIterator = children.fields().iterator();
-        BasicDocument testDocument = new BasicDocument(KEY_ID, childNodeKeyString, "name", childNodeKeyName);
-        List<Document.Field> fieldList = IteratorUtils.toList(fieldsIterator);
-
-
-        boolean foundMatch = false;
-        Document persistedFedoraDocument = null;
-        for (int i = 0; i < fieldList.size(); i++) {
-            if (fieldList.get(i).getValueAsDocument().containsAll(testDocument)) {
-                persistedFedoraDocument = fieldList.get(i).getValueAsDocument();
-                foundMatch = true;
-            }
-        }
-
-        LOGGER.debug("\n\nOK. Done reading Fedora repo node(s).\n\n");
-        return "N/A";
-    }
-
-
-
-
 }
